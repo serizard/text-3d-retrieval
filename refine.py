cc48f93dda2c9154af1622ffdae89ae00708fd17
serizard <serizard1005@gmail.com>
2024-07-26 13:46:30 +0900

    Inital commit

---
+++ refine.py
@@ -0,0 +1,36 @@
+from transformers import pipeline
+
+class TextRefiner:
+    def __init__(self, model_id="meta-llama/Meta-Llama-3-8B-Instruct"):
+
+        self.text_generator = pipeline(
+            "text-generation",
+            model=model_id,
+            tokenizer=model_id,
+            token = 'dfdf',
+            model_kwargs={"torch_dtype": "bfloat16"},
+            device_map ='auto'
+        )
+
+    def refine(self, user_input):
+        prompt_template = f"""
+        I want you to transform the following user description into a structured text suitable for CLIP text input for a text-to-3D retrieval task.
+        If the user description is written in other languages than English, please translate it to English before refining.
+        
+        User description: {user_input}
+        
+        Structured description for CLIP input: 
+        """
+
+        output = self.text_generator(prompt_template, max_length=200, do_sample=True, temperature=0.7)
+
+
+        structured_text = output[0]['generated_text'].replace(prompt_template, "").strip()
+
+        return structured_text
+
+if __name__ == "__main__":
+    refiner = TextRefiner()
+    user_input = "A person is standing in front of a building."
+    structured_text = refiner.refine(user_input)
+    print(structured_text)

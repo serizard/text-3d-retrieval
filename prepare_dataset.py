9169e1dcc83c341586178e37b5257b18bfcb38b4
serizard <serizard@gmail.com>
2024-08-05 04:36:44 +0000

    updated

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -16,7 +16,22 @@ import torch
 import model.models as models
 import pickle
 from huggingface_hub import hf_hub_download
-from utils.misc import dump_config
+
+
+
+
+def correct_off_header(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    if len(lines[0].split('OFF')) > 2:
+        header = lines[0].split('OFF')
+        lines[0] = 'OFF\n'
+        lines.insert(1, ' '.join(header[1:]).strip() + '\n')
+
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
+
 
 def move_off_files(src_dir, dest_dir):
     """Move .off files from the source directory to the destination directory."""
@@ -28,6 +43,7 @@ def move_off_files(src_dir, dest_dir):
         dest_file_path = osp.join(dest_dir, osp.basename(file_path))
         shutil.move(file_path, dest_file_path)
 
+
 def download_modelnet10(data_dir):
     def show_progress(block_num, block_size, total_size):
         downloaded = block_num * block_size
@@ -35,25 +51,25 @@ def download_modelnet10(data_dir):
         print(f"\rProgress: {progress:.2f}%", end="")
 
     os.makedirs(data_dir, exist_ok=True)
-    """Download and extract the ModelNet10 dataset."""
-    url = 'http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip'
-    zip_path = osp.join(data_dir, 'ModelNet10.zip')
+    """Download and extract the ModelNet40 dataset."""
+    url = 'http://modelnet.cs.princeton.edu/ModelNet40.zip'
+    zip_path = osp.join(data_dir, 'ModelNet40.zip')
 
     if osp.exists(osp.join(data_dir, 'meshes')):
-        print("ModelNet10 dataset already exists.")
+        print("ModelNet40 dataset already exists.")
         return
     
     if osp.exists(zip_path):
-        print("ModelNet10 zip file already exists.")
+        print("ModelNet40 zip file already exists.")
     else:
-        print("Downloading ModelNet10 dataset...")
+        print("Downloading ModelNet40 dataset...")
         try:
             urllib.request.urlretrieve(url, zip_path, show_progress)
             print("\nDownload complete.")
         except Exception as e:
-            raise Exception(f"Failed to download ModelNet10 dataset: {e}")
+            raise Exception(f"Failed to download ModelNet40 dataset: {e}")
 
-    print("Extracting ModelNet10 dataset...")
+    print("Extracting ModelNet40 dataset...")
     try:
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall(data_dir)
@@ -63,16 +79,22 @@ def download_modelnet10(data_dir):
         if not osp.exists(mesh_path):
             os.makedirs(mesh_path)
 
-        move_off_files(osp.join(data_dir, 'ModelNet10'), mesh_path)
+        move_off_files(osp.join(data_dir, 'ModelNet40'), mesh_path)
         print("Extraction complete.")
     except Exception as e:
-        raise Exception(f"Failed to extract ModelNet10 dataset: {e}")
+        raise Exception(f"Failed to extract ModelNet40 dataset: {e}")
     finally:
-        shutil.rmtree(osp.join(data_dir, 'ModelNet10'))
+        shutil.rmtree(osp.join(data_dir, 'ModelNet40'))
+
 
 def to_pcd(off_path, num_points):
     """Convert an OFF file to a point cloud with a given number of points."""
-    mesh = o3d.io.read_triangle_mesh(off_path)
+    correct_off_header(off_path)
+    try:
+        mesh = o3d.io.read_triangle_mesh(off_path)
+    except:
+        print(f"Failed to read {off_path}")
+        return None
     return mesh.sample_points_uniformly(number_of_points=num_points)
 
 def load_model(config):
@@ -101,6 +123,7 @@ def load_model(config):
     model.load_state_dict(model_dict)
     return model
 
+
 def preprocess_modelnet10(model, configs, y_up=True):
     """Convert all OFF files in the dataset to point cloud files and extract features."""
     off_files = glob(osp.join(configs.data_dir, 'meshes', '*.off'), recursive=True)
@@ -109,32 +132,45 @@ def preprocess_modelnet10(model, configs, y_up=True):
 
     embedded_features = {}
     os.makedirs('./modelnet_embed', exist_ok=True)
-    for off_file in tqdm(off_files, desc='Converting the dataset type'):
-        pcd = to_pcd(off_file, configs.num_points)
-        xyz = np.asarray(pcd.points)
-        rgb = np.asarray(pcd.colors)
-        if y_up:
-            xyz[:, [1, 2]] = xyz[:, [2, 1]]
-        xyz = normalize_pc(xyz)
+    for off_file in tqdm(off_files, desc='Encoding the dataset'):
+        if osp.exists('./modelnet_embed/modelnet.pkl'):
+            with open('./modelnet_embed/modelnet.pkl', 'rb') as f:
+                try:
+                    embedded_features = pickle.load(f)
+                    if off_file in embedded_features.keys():
+                        continue
+                except:
+                    pass
+        try:
+            pcd = to_pcd(off_file, configs.num_points)
+            xyz = np.asarray(pcd.points)
+            rgb = np.asarray(pcd.colors)
+            if y_up:
+                xyz[:, [1, 2]] = xyz[:, [2, 1]]
+            xyz = normalize_pc(xyz)
 
-        if rgb is None or len(rgb) == 0:
-            rgb = np.ones_like(xyz) * 0.4
+            if rgb is None or len(rgb) == 0:
+                rgb = np.ones_like(xyz) * 0.4
 
-        features = np.concatenate([xyz, rgb], axis=1)
-        xyz_tensor = torch.tensor(xyz, dtype=torch.float32).unsqueeze(0).cuda()
-        features_tensor = torch.tensor(features, dtype=torch.float32).unsqueeze(0).cuda()
+            features = np.concatenate([xyz, rgb], axis=1)
+            xyz_tensor = torch.tensor(xyz, dtype=torch.float32).unsqueeze(0).cuda()
+            features_tensor = torch.tensor(features, dtype=torch.float32).unsqueeze(0).cuda()
 
-        shape_feature = model(xyz_tensor, features_tensor, quantization_size=config.model.voxel_size)
-        embedded_features[off_file] = shape_feature.cpu().detach().numpy()[0]
+            shape_feature = model(xyz_tensor, features_tensor, quantization_size=config.model.voxel_size)
+            embedded_features[off_file] = shape_feature.cpu().detach().numpy()[0]
 
-        with open('./modelnet_embed/modelnet.pkl', 'wb') as f:
-            pickle.dump(embedded_features, f)
+            with open('./modelnet_embed/modelnet.pkl', 'wb') as f:
+                pickle.dump(embedded_features, f)
+        except:
+            print(f"Failed to convert {off_file}")
+            continue
 
     print("Preprocessing complete.")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download and preprocess ModelNet10 dataset')
-    parser.add_argument('--download_dir', type=str, default='./data', help='Directory to download ModelNet10 dataset')
+    parser.add_argument('--download_dir', type=str, default='./data', help='Directory to download ModelNet40 dataset')
     args = parser.parse_args()
 
     config = load_config()
